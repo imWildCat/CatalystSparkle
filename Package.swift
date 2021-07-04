4c846c94e345f6740b73cb0a8a54f6aafce4af8f
WildCat <wildcat.name@gmail.com>
2021-07-04 21:07:28 +0800

    Fix xcframework

--- Package.swift
+++ Package.swift
@@ -2,10 +2,10 @@
 import PackageDescription
 
 // Version is technically not required here, SPM doesn't check
-let version = "2.0.0-local"
+let version = "2.0.0-local-2"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
-let tag = "2.0.0-local"
-let checksum = "3f1c04830e45481218d79b71fee7b1be3b5008456dc5c104c3002a20907f84e4"
+let tag = "2.0.0-local-2"
+let checksum = "1613c1c81737658e77dbeb639af474362494e66811ec26cad2bb70e48b7f5ebe"
 let url = "https://github.com/imWildCat/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(

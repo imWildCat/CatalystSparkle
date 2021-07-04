b80777acc8fd6eb5e0e1da1cd1714ef701d87a38
WildCat <wildcat.name@gmail.com>
2021-07-04 20:59:47 +0800

    Refine SPM

--- Package.swift
+++ Package.swift
@@ -2,11 +2,11 @@
 import PackageDescription
 
 // Version is technically not required here, SPM doesn't check
-let version = "2.0.0"
+let version = "2.0.0-local"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
-let tag = "2.0.0"
-let checksum = "ef0d0cc46421f8644f455b62537f4e4db2c97bdca5820641219a1836f3782878"
-let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
+let tag = "2.0.0-local"
+let checksum = "3f1c04830e45481218d79b71fee7b1be3b5008456dc5c104c3002a20907f84e4"
+let url = "https://github.com/imWildCat/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
     name: "Sparkle",
